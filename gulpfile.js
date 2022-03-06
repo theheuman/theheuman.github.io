c5dc728203d323222e32dd3d9b9d52e790f82fa1
theheuman <theheuman@users.noreply.github.com>
2022-03-06 14:57:56 -0500

    Task/update dependencies (#12)
    * update dependencies
    
    * add husky, prettier, eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,137 +1,136 @@
-"use strict";
+'use strict'
 
 // Load plugins
-const autoprefixer = require("gulp-autoprefixer");
-const browsersync = require("browser-sync").create();
-const cleanCSS = require("gulp-clean-css");
-const del = require("del");
-const gulp = require("gulp");
-const header = require("gulp-header");
-const merge = require("merge-stream");
-const plumber = require("gulp-plumber");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-const uglify = require("gulp-uglify");
-
-// Load package.json for banner
-const pkg = require('./package.json');
-
-// Set the banner content
-const banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
-].join('');
+const autoprefixer = require('gulp-autoprefixer')
+const browsersync = require('browser-sync').create()
+const cleanCSS = require('gulp-clean-css')
+const del = require('del')
+const gulp = require('gulp')
+const merge = require('merge-stream')
+const plumber = require('gulp-plumber')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify')
 
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: "./"
+      baseDir: './',
     },
-    port: 3000
-  });
-  done();
+    port: 3000,
+  })
+  done()
 }
 
 // BrowserSync reload
 function browserSyncReload(done) {
-  browsersync.reload();
-  done();
+  browsersync.reload()
+  done()
 }
 
 // Clean vendor
 function clean() {
-  return del(["./vendor/"]);
+  return del(['./vendor/'])
 }
 
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
   // Bootstrap
-  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
+  const bootstrap = gulp
+    .src('./node_modules/bootstrap/dist/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap'))
   // Font Awesome CSS
-  var fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
-    .pipe(gulp.dest('./vendor/fontawesome-free/css'));
+  const fontAwesomeCSS = gulp
+    .src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
+    .pipe(gulp.dest('./vendor/fontawesome-free/css'))
   // Font Awesome Webfonts
-  var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-    .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
+  const fontAwesomeWebfonts = gulp
+    .src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
+    .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'))
   // jQuery Easing
-  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-    .pipe(gulp.dest('./vendor/jquery-easing'));
+  const jqueryEasing = gulp
+    .src('./node_modules/jquery.easing/*.js')
+    .pipe(gulp.dest('./vendor/jquery-easing'))
   // jQuery
-  var jquery = gulp.src([
+  const jquery = gulp
+    .src([
       './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
+      '!./node_modules/jquery/dist/core.js',
     ])
-    .pipe(gulp.dest('./vendor/jquery'));
-  var mfizz = gulp.src('./node_modules/mfizz/*')
-    .pipe(gulp.dest('./vendor/font-mfizz-2.4.1'));
-  return merge(bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jquery, jqueryEasing, mfizz);
+    .pipe(gulp.dest('./vendor/jquery'))
+  const mfizz = gulp
+    .src('./node_modules/mfizz/*')
+    .pipe(gulp.dest('./vendor/font-mfizz-2.4.1'))
+  return merge(
+    bootstrap,
+    fontAwesomeCSS,
+    fontAwesomeWebfonts,
+    jquery,
+    jqueryEasing,
+    mfizz
+  )
 }
 
 // CSS task
 function css() {
   return gulp
-    .src("./scss/**/*.scss")
+    .src('./scss/**/*.scss')
     .pipe(plumber())
-    .pipe(sass({
-      outputStyle: "expanded",
-      includePaths: "./node_modules",
-    }))
-    .on("error", sass.logError)
-    .pipe(autoprefixer({
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest("./css"))
-    .pipe(rename({
-      suffix: ".min"
-    }))
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+        includePaths: './node_modules',
+      })
+    )
+    .on('error', sass.logError)
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest('./css'))
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
     .pipe(cleanCSS())
-    .pipe(gulp.dest("./css"))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('./css'))
+    .pipe(browsersync.stream())
 }
 
 // JS task
 function js() {
   return gulp
-    .src([
-      './js/*.js',
-      '!./js/*.min.js'
-    ])
+    .src(['./js/*.js', '!./js/*.min.js'])
     .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
     .pipe(gulp.dest('./js'))
-    .pipe(browsersync.stream());
+    .pipe(browsersync.stream())
 }
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./scss/**/*", css);
-  gulp.watch(["./js/**/*", "!./js/**/*.min.js"], js);
-  gulp.watch("./**/*.html", browserSyncReload);
+  gulp.watch('./scss/**/*', css)
+  gulp.watch(['./js/**/*', '!./js/**/*.min.js'], js)
+  gulp.watch('./**/*.html', browserSyncReload)
 }
 
 // Define complex tasks
-const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
-const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+const vendor = gulp.series(clean, modules)
+const build = gulp.series(vendor, gulp.parallel(css, js))
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync))
 
 // Export tasks
-exports.css = css;
-exports.js = js;
-exports.clean = clean;
-exports.vendor = vendor;
-exports.build = build;
-exports.watch = watch;
-exports.default = build;
+exports.css = css
+exports.js = js
+exports.clean = clean
+exports.vendor = vendor
+exports.build = build
+exports.watch = watch
+exports.default = build
